d9fe8344dfb830abebe7f24c75acc59853ae94c8
kingsanguch <sangurajohnnie17@gmail.com>
2024-02-24 21:40:29 -0800

    0x05. Processes and signals

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void)
+{
+    pid_t child_pid;
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        child_pid = fork();
+        if (child_pid > 0)
+        {
+            printf("Zombie process created, PID: %d\n", child_pid);
+            sleep(1);
+        }
+        else
+        {
+            exit(0);
+        }
+    }
+
+    infinite_while();
+    return (0);
+}
